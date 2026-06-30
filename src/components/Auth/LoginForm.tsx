9f414940880a36115ebf5ec5b008e02c934b2ce2
Arko Gain <37438306+arkogain@users.noreply.github.com>
2026-06-30 14:04:36 +0530

    login & register ui

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,5 +1,95 @@
+"use client";
+
+import { loginSchema, LoginSchemaType } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FingerprintIcon, LoaderIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "../shadcnui/button";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+
 const LoginForm = () => {
-  return <></>;
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+    mode: "all",
+  });
+
+  const loginHandler = async ({}: LoginSchemaType) => {
+    console.log("yahu !");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(loginHandler)}
+      className="grid gap-4"
+      noValidate>
+      {/* Email */}
+
+      <Controller
+        name="email"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Email Address</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="email"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your email"
+              autoComplete="email"
+            />
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      {/* Password */}
+
+      <Controller
+        name="password"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="password"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your password"
+              autoComplete="new-password"
+            />
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      <Button
+        type="submit"
+        disabled={isSubmitting}>
+        {isSubmitting ?
+          <>
+            <LoaderIcon className="animate-spin" /> Logining...
+          </>
+        : <>
+            <FingerprintIcon /> Login
+          </>
+        }
+      </Button>
+    </form>
+  );
 };
 
 export default LoginForm;
--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,5 +1,139 @@
+"use client";
+
+import { registerSchema, RegisterSchemaType } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoaderIcon, UserPlusIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "../shadcnui/button";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+
 const RegisterForm = () => {
-  return <></>;
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "all",
+  });
+
+  const registerHandler = async ({}: RegisterSchemaType) => {
+    console.log("yahu !");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(registerHandler)}
+      className="grid gap-4"
+      noValidate>
+      {/* fullName Field */}
+      <Controller
+        name="name"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Full Name</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="text"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your full name"
+              autoComplete="name"
+            />
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      {/* Email */}
+
+      <Controller
+        name="email"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Email Address</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="email"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your email"
+              autoComplete="email"
+            />
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      {/* Password */}
+
+      <Controller
+        name="password"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="password"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your password"
+              autoComplete="new-password"
+            />
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      {/* confirm password */}
+
+      <Controller
+        name="confirmPassword"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Confirm password</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="password"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your confirm password"
+              autoComplete="new-password"
+            />
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      <Button
+        type="submit"
+        disabled={isSubmitting}>
+        {isSubmitting ?
+          <>
+            <LoaderIcon className="animate-spin" /> Submiting...
+          </>
+        : <>
+            <UserPlusIcon /> Register
+          </>
+        }
+      </Button>
+    </form>
+  );
 };
 
 export default RegisterForm;
